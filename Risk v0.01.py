64afcae023d10e0d51baf607041b1b34ee31d725
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2019-08-17 00:04:32 +0200

    add classes: Map, Continent, Region
    classes to describe game objects and instantiate the start of the game playfield thingy.

--- Risk v0.01.py
+++ Risk v0.01.py
@@ -1,1 +1,50 @@
+
 # Risk will be here
+
+
+Continents = {"Noord-Amerika":["Alaska","Quebec"],
+              "Zuid-Amerika":["Brazilië","Peru"]}
+
+class Map():
+    """ """
+    def __init__(self):
+        self.name = "RISK"
+        self.continents = list()
+        self.create_continents()
+
+
+    def create_continents(self):
+        for continent_name, regions in Continents.items():
+            continent = Continent(continent_name, regions)
+            self.continents.append(continent)
+
+
+class Continent():
+    """ """
+    def __init__(self, name, regions):
+        self.name = name
+        self.regions = list()
+        self.create_regions(regions)
+
+    def create_regions(self, regions):
+        for region_name in regions:
+            region = Region(region_name)
+            self.regions.append(region)
+
+
+class Region():
+    """ """
+    def __init__(self, name):
+        self.name = name
+
+
+# test the classes
+def printMap():
+    mapobj = Map()
+    for continent in mapobj.continents:
+        print(continent.name)
+        for region in continent.regions:
+            print("  ",region.name)
+
+
+printMap()
